928f9a07c10db76e4381e6984ba48e370f7e7b37
niks2010 <niks.sbg@gmail.com>
2018-08-22 21:33:31 +0530

    dynamic content template working

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from django.template.loader import get_template
 from django.http import HttpResponse
+import datetime
 
 from django.views.decorators.csrf import csrf_protect
 
 @csrf_protect
 def home(request):
-    return render(request, "home.html")
+    t = get_template('home.html')
+    message = ["hi betty", "this is jarvis", "good to go", "hasta la vista"]
+    html = t.render({'message': message})
+    return HttpResponse(html)
 
+def fillChat(request):
+    return {
+        "message": "Hello from the other side."
+    }
 
 def jarvis(request):
+    now = datetime.datetime.now()
     return HttpResponse(
-        "<div align='center'><h1>Hello jarvis here!</h1></div>")
+        "<div align='center'><h1>Hello, jarvis here!</h1> <p>Time now is %s</p></div>" % now)
--- jarvis/settings.py
+++ jarvis/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'chatbot',
 ]
-
+    
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
