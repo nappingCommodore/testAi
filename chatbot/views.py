170fe5943d2ed79fbaf7c0074942a33e7a6c43b1
niks2010 <niks.sbg@gmail.com>
2018-08-27 23:20:35 +0530

    added sqlite3 table

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,19 +2,21 @@ from django.shortcuts import render
 from django.template.loader import get_template
 from django.http import HttpResponse
 import datetime
+import sqlite3
 
 from django.views.decorators.csrf import csrf_protect
-
-
-
 
 message = ["Hello, how are you?"]
 # userOrBot = "bot"
 
 @csrf_protect
 def home(request):
+    conn = sqlite3.connect('../dq.sqlite3')
+    cursor = conn.cursor()
     if request.method == 'POST':
         msg = request.POST.get("msg")
+        insertVal = (msg, "you")
+        cursor.execute("INSERT INTO chats(msg, who) VALUES(?, ?)", insertVal)
         message.append(msg)
         userOrBot = "you"
         return render(request, 'home.html', {'message': message},{'userOrBot': userOrBot})
@@ -24,3 +26,5 @@ def home(request):
         # html = t.render({'message': message})
         userOrBot = "bot"
         return render(request, 'home.html', {'message': message},{'userOrBot': userOrBot})
+    conn.commit()
+    conn.close()
