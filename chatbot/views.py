1628127ff0a403f4d6474cbeba55cd8c141ea2a0
niks2010 <niks.sbg@gmail.com>
2018-08-28 23:28:48 +0530

    added sqlite db, data insertion working

--- chatbot/views.py
+++ chatbot/views.py
@@ -11,20 +11,22 @@ message = ["Hello, how are you?"]
 
 @csrf_protect
 def home(request):
-    conn = sqlite3.connect('../dq.sqlite3')
+    conn = sqlite3.connect('db.sqlite3')
     cursor = conn.cursor()
     if request.method == 'POST':
         msg = request.POST.get("msg")
         insertVal = (msg, "you")
-        cursor.execute("INSERT INTO chats(msg, who) VALUES(?, ?)", insertVal)
+        try:
+            cursor.execute("INSERT INTO chats(msg, who) VALUES(?, ?)", insertVal,)
+            conn.commit()
+        except Exception as e:
+            print(e)
+        
         message.append(msg)
-        userOrBot = "you"
-        return render(request, 'home.html', {'message': message},{'userOrBot': userOrBot})
+        return render(request, 'home.html', {'message': message})
     else:
         # t = get_template('home.html')
         # message = ["hi betty", "this is jarvis", "good to go", "hasta la vista"]
         # html = t.render({'message': message})
-        userOrBot = "bot"
-        return render(request, 'home.html', {'message': message},{'userOrBot': userOrBot})
-    conn.commit()
+        return render(request, 'home.html', {'message': message})
     conn.close()
