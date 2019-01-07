2a1c90fa1ea06302eef1a935afbb66a68be702d3
niks2010 <niks.sbg@gmail.com>
2019-01-08 05:21:31 +0530

    setting change

--- jarvis/settings.py
+++ jarvis/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'jarvis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # LOCAL_SQLITE='sqlite:///' + os.path.abspath(os.path.join(BASE_DIR, '../database/db.sqlite3'))
 # DATABASES = {}
